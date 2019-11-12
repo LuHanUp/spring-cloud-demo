bed6e8be2280a529f1b7f1790ac662f8e5fa2ceb
luHan <luhan@caiyikeji.com>
2019-11-12 17:19:46 +0800

    commit config server

---
+++ config-server/src/main/java/top/luhancc/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package top.luhancc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * 〈SpringCloud Config Server〉<br>
+ *
+ * @author luHan
+ * @create 2019-11-12 16:51
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
